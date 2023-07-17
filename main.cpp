d8841bd96d4dabf3b838f5cce0093eda1c969e06
leowrites <75044178+leowrites@users.noreply.github.com>
2023-07-17 12:57:46 -0400

    Add switch case for rendering/handling different game states

--- main.cpp
+++ main.cpp
@@ -4,22 +4,46 @@
 
 #include <iostream>
 #include "raylib.h"
+#include "entity/Game.h"
+#include "view/HomeView.h"
+#include "view/LoadingView.h"
+#include "view/GameConfigView.h"
+
 int main()
 {
     std::cout << "Hello World" << std::endl;
     const int screenWidth{ 800 };
     const int screenHeight{ 800 };
-
-    InitWindow(screenWidth, screenHeight, "Hello World!");
+    Vector2 mousePosition;
+    Chess::GameState currGameState { Chess::GameState::Home };
+    InitWindow(screenWidth, screenHeight, "Chess By Leo V2");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        mousePosition = GetMousePosition();
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        // PlayerMouseDragSystem
-        // MoveSystem
-        // RenderSystem
+        switch (currGameState)
+        {
+            case Chess::GameState::Loading:
+                Chess::LoadingView::Render(screenWidth, screenHeight);
+                break;
+            case Chess::GameState::Home:
+                Chess::HomeView::Render(screenWidth, screenHeight, mousePosition);
+                Chess::HomeView::HandleMouseClick(mousePosition, currGameState);
+                break;
+            case Chess::GameState::Configuration:
+                break;
+            case Chess::GameState::InGame:
+                break;
+            case Chess::GameState::Paused:
+                break;
+            case Chess::GameState::GameOver:
+                break;
+            case Chess::GameState::Statistics:
+                break;
+        }
 
         EndDrawing();
     }
