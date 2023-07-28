bef8f0c79b582c11c75d8cb6fcc9a24bd75e6e3b
leowrites <75044178+leowrites@users.noreply.github.com>
2023-07-28 09:43:41 -0400

    Initialize board with all pawns

--- entity/board/Board.cpp
+++ entity/board/Board.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Board.h"
+
+Chess::Board::Board()
+{
+    // initialize the pieces here
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 8; ++col) {
+            m_board[row][col] = std::make_shared<Pawn>(ChessPieceType::pawn, PlayerRole::White,
+                                                       "../texture/whitePawnIcon.png");;
+        }
+    }
+}
--- entity/board/Board.h
+++ entity/board/Board.h
@@ -4,9 +4,24 @@
 
 #pragma once
 
+#include <array>
+#include "entity/piece/Piece.h"
+#include "entity/piece/Pawn.h"
+
 namespace Chess
 {
+    using BoardMatrix = std::array<std::array<std::shared_ptr<Piece>, 8>, 8>;
     class Board {
-
+    private:
+        // how to represent the chess board?
+        // pieces/squares/integer
+        // there may not be needs for squares
+        BoardMatrix m_board { nullptr };
+        // render: render black/white depending on square row/col
+        //          render a piece if Piece is not null
+        // update:
+    public:
+        Board();
+        BoardMatrix GetBoardMatrix() { return m_board; };
     };
 }
