34d836d8449914cb7ed8de2563d11582e877a54d
leowrites <75044178+leowrites@users.noreply.github.com>
2023-07-28 20:36:14 -0400

    convert to compile time constants

--- utility/utility.h
+++ utility/utility.h
@@ -9,15 +9,16 @@
 
 namespace Chess
 {
-    const int SCREEN_WIDTH { 800 };
-    const int SCREEN_HEIGHT { 800 };
-    const int SCREEN_PADDING_X { SCREEN_WIDTH / 16 };
-    const int SCREEN_PADDING_Y { SCREEN_HEIGHT / 16 };
-    const int TITLE_TEXT_FONT_SIZE { 50 };
-    const int HOME_MENU_TEXT_FONT_SIZE { 30 };
-    const int MENU_SPACING { 20 };
-    const Color BOARD_LIGHT_TILE { 103, 101, 47, 100 };
-    const Color BOARD_DARK_TILE { 255, 225, 206, 100 };
+    constexpr int SCREEN_WIDTH { 800 };
+    constexpr int SCREEN_HEIGHT { 800 };
+    constexpr int SCREEN_PADDING_X { SCREEN_WIDTH / 16 };
+    constexpr int SCREEN_PADDING_Y { SCREEN_HEIGHT / 16 };
+    constexpr int TITLE_TEXT_FONT_SIZE { 50 };
+    constexpr int HOME_MENU_TEXT_FONT_SIZE { 30 };
+    constexpr int SQUARE_PIXEL_SIZE { SCREEN_WIDTH / 8 };
+    constexpr int MENU_SPACING { 20 };
+    constexpr Color BOARD_LIGHT_TILE { 103, 101, 47, 100 };
+    constexpr Color BOARD_DARK_TILE { 255, 225, 206, 100 };
     enum class HOME_MENU_OPTIONS
     {
         SinglePlayer, AgainstComputer, Stats, Quit
