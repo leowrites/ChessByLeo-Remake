15a68c82245f7798ac76aa37b3c811e716545a2a
leowrites <75044178+leowrites@users.noreply.github.com>
2023-08-06 15:17:01 -0400

    Add functions to generate all possible positions for a piece

--- entity/board/Board.cpp
+++ entity/board/Board.cpp
@@ -149,4 +149,195 @@ namespace Chess
         // use the piece's old position and set that to nullptr
         m_board[newPos->second][newPos->first] = std::move(m_board[oldPos->second][oldPos->first]);
     }
+    std::unique_ptr<std::vector<GridPosPtr>> Board::GetAllPossibleHorizontalAndVerticalPositions(const std::shared_ptr<Piece> &piece)
+    {
+        GridPosPtr currentPos { std::move(CalculateGridPosGivenCoord(piece->GetPosition())) };
+        int startX { currentPos->first };
+        int startY { currentPos->second };
+        std::unique_ptr<std::vector<GridPosPtr>> positions { std::make_unique<std::vector<GridPosPtr>>() };
+//        LoopAndAddGridPosToVector(
+//                startX, startY, 1, 1, 8, 8, 1, 0, positions, piece->GetPieceOwner()
+//                );
+        for (int i = 1; startX + i < 8; ++i) {
+            if (m_board[currentPos->second][startX + i] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(startX + i, currentPos->second));
+            } else if (m_board[currentPos->second][startX + i]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(startX + i, currentPos->second));
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; startX - i >= 0; ++i) {
+            if (m_board[currentPos->second][startX - i] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(startX - i, currentPos->second));
+            } else if (m_board[currentPos->second][startX - i]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(startX - i, currentPos->second));
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; startY + i < 8; ++i) {
+            if (m_board[startY + i][currentPos->first] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(currentPos->first, startY + i));
+            } else if (m_board[startY + i][currentPos->first]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(currentPos->first, startY + i));
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; startY - i >= 0; ++i) {
+            if (m_board[startY - i][currentPos->first] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(currentPos->first, startY - i));
+            } else if (m_board[startY - i][currentPos->first]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(currentPos->first, startY - i));
+                break;
+            } else {
+                break;
+            }
+        }
+        for (auto& pos: *positions)
+        {
+            std::cout << "Grid(" << (int)pos->first << ", " << (int)pos->second << ")" << std::endl;
+        }
+        return positions;
+    }
+    std::unique_ptr<std::vector<GridPosPtr>> Board::GetAllPossibleDiagonalPositions(const std::shared_ptr<Piece> &piece)
+    {
+        GridPosPtr currentPos { std::move(CalculateGridPosGivenCoord(piece->GetPosition())) };
+        int startX { currentPos->first };
+        int startY { currentPos->second };
+        int currX;
+        int currY;
+        std::unique_ptr<std::vector<GridPosPtr>> positions { std::make_unique<std::vector<GridPosPtr>>() };
+        // bottom right
+        for (int i = 1, j = 1; startX + i < 8 && startY + i < 8; ++i, ++j) {
+            currX = startX + i;
+            currY = startY + j;
+            if (m_board[currY][currX] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+            } else if (m_board[currY][currX]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+                break;
+            } else {
+                break;
+            }
+        }
+        // top right
+        for (int i = 1, j = 1; startX + i < 8 && startY - j >= 0; ++i, ++j) {
+            currX = startX + i;
+            currY = startY - j;
+            if (m_board[currY][currX] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+            } else if (m_board[currY][currX]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+                break;
+            } else {
+                break;
+            }
+        }
+        // top left
+        for (int i = 1, j = 1; startX - i >= 0 && startY - j >= 8; ++i, ++j) {
+            currX = startX - i;
+            currY = startY - j;
+            if (m_board[currY][currX] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+            } else if (m_board[currY][currX]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+                break;
+            } else {
+                break;
+            }
+        }
+        // bottom left
+        for (int i = 1, j = 1; startX - i >= 0 && startX + j < 8; ++i, ++j) {
+            currX = startX - i ;
+            currY = startY + j ;
+            if (m_board[currY][currX] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+            } else if (m_board[currY][currX]->GetPieceOwner() != piece->GetPieceOwner()) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+                break;
+            } else {
+                break;
+            }
+        }
+        for (auto& pos: *positions)
+        {
+            std::cout << "Grid(" << (int)pos->first << ", " << (int)pos->second << ")" << std::endl;
+        }
+        return positions;
+    }
+    std::unique_ptr<std::vector<GridPosPtr>> Board::GetAllPossiblePositionsForPiece(const std::shared_ptr<Piece> &piece) {
+        GridPosPtr gridPosPtr { std::move(CalculateGridPosGivenCoord(piece->GetPosition()))};
+        switch(piece->GetPieceType())
+        {
+            case ChessPieceType::rook:
+            {
+                return GetAllPossibleHorizontalAndVerticalPositions(piece);
+            }
+            case ChessPieceType::bishop:
+            {
+                std::cout << "Bishop"<< std::endl;
+                return GetAllPossibleDiagonalPositions(piece);
+            }
+            case ChessPieceType::queen:
+            {
+                std::cout << "Queen"<< std::endl;
+                std::unique_ptr<std::vector<GridPosPtr>> vec1 { std::move(GetAllPossibleHorizontalAndVerticalPositions(piece)) };
+                std::unique_ptr<std::vector<GridPosPtr>> vec2 { std::move(GetAllPossibleDiagonalPositions(piece)) };
+                vec1->insert(vec1->end(), std::make_move_iterator(vec2->begin()), std::make_move_iterator(vec2->end()));
+                return vec1;
+            }
+            case ChessPieceType::knight:
+            {
+                std::unique_ptr<std::vector<GridPosPtr>> posPtr { std::make_unique<std::vector<GridPosPtr>>() };
+                std::array possiblePos {
+                        GridPos {gridPosPtr->first - 1, gridPosPtr->second + 2},
+                        GridPos {gridPosPtr->first + 1, gridPosPtr->second + 2},
+                        GridPos {gridPosPtr->first - 1, gridPosPtr->second - 2},
+                        GridPos {gridPosPtr->first + 1, gridPosPtr->second - 2},
+                        GridPos {gridPosPtr->first - 2, gridPosPtr->second + 1},
+                        GridPos {gridPosPtr->first + 2, gridPosPtr->second + 1},
+                        GridPos {gridPosPtr->first - 2, gridPosPtr->second - 1},
+                        GridPos {gridPosPtr->first + 2, gridPosPtr->second - 1}
+                };
+                for (auto& pos: possiblePos)
+                {
+                    posPtr->push_back(std::make_unique<GridPos>(pos));
+                }
+                return posPtr;
+            }
+            case ChessPieceType::pawn:
+            {
+                return std::make_unique<std::vector<GridPosPtr>>();
+            }
+            case ChessPieceType::king:
+                return nullptr;
+        }
+    }
+    void Board::LoopAndAddGridPosToVector(int startX, int startY, int startCountX, int startCountY, int endX, int endY,
+                                          int incX, int incY, std::unique_ptr<std::vector<GridPosPtr>>& positions, PlayerRole pieceOwner)
+    {
+        // to go from left to right
+        // endX > startX, inc by 1
+        // stop when startX + i == 8
+        // to go from right to left
+        // if endX < startX, dec by 1
+        // stop when startX - i == 0
+        for (int i = startCountX, j = startCountY; startX + i != endX && startY + j != endY; i += incX, j += incY) {
+            int currX { startX + i };
+            int currY { startY + j };
+            if (m_board[currY][currX] == nullptr) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+            } else if (m_board[currY][currX]->GetPieceOwner() != pieceOwner) {
+                positions->push_back(std::make_unique<GridPos>(currX, currY));
+                break;
+            } else {
+                break;
+            }
+        }
+    }
 }
--- entity/board/Board.h
+++ entity/board/Board.h
@@ -14,6 +14,7 @@
 namespace Chess
 {
     using BoardMatrix = std::array<std::array<std::shared_ptr<Piece>, 8>, 8>;
+    using PlayerPieces = std::map<ChessPieceType, std::unordered_set<std::shared_ptr<Piece>>>;
     class Board {
     private:
         BoardMatrix m_board { nullptr };
@@ -22,15 +23,19 @@ namespace Chess
         std::map<ChessPieceType, std::unordered_set<std::shared_ptr<Piece>>> m_whitePieces;
         std::map<ChessPieceType, std::unordered_set<std::shared_ptr<Piece>>> m_blackPieces;
         void InitializeChessPieces(PlayerRole playerRole);
+        void LoopAndAddGridPosToVector(int startX, int startY, int startCountX, int startCountY, int endX, int endY,
+                                       int incX, int incY, std::unique_ptr<std::vector<GridPosPtr>>& positions, PlayerRole pieceOwner);
     public:
         Board();
         BoardMatrix& GetBoardMatrix() { return m_board; };
         const std::shared_ptr<Piece>& GetWhiteKing() { return m_whiteKing; };
         const std::shared_ptr<Piece>& GetBlackKing() { return m_blackKing; };
-        std::map<ChessPieceType, std::unordered_set<std::shared_ptr<Piece>>>& GetWhitePieces() { return m_whitePieces; };
-        std::map<ChessPieceType, std::unordered_set<std::shared_ptr<Piece>>>& GetBlackPieces() { return m_blackPieces; };
+        PlayerPieces& GetWhitePieces() { return m_whitePieces; };
+        PlayerPieces& GetBlackPieces() { return m_blackPieces; };
         void InsertPieceIntoPieces(const std::shared_ptr<Piece>& piece);
-
+        std::unique_ptr<std::vector<GridPosPtr>> GetAllPossiblePositionsForPiece(const std::shared_ptr<Piece> &piece);
+        std::unique_ptr<std::vector<GridPosPtr>> GetAllPossibleHorizontalAndVerticalPositions(const std::shared_ptr<Piece> &piece);
+        std::unique_ptr<std::vector<GridPosPtr>> GetAllPossibleDiagonalPositions(const std::shared_ptr<Piece> &piece);
         void UpdatePiecePositionInBoard(std::shared_ptr<Piece>& piece, GridPosPtr& newPos, GridPosPtr& oldPos);
     };
 }
