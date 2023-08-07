0c42c4e550a88243bd932254b6fa33fa08c22a28
leowrites <75044178+leowrites@users.noreply.github.com>
2023-08-07 01:49:29 -0400

    Add functions to get possible positions for each piece

--- utility/check_validator/CheckmateValidator.h
+++ utility/check_validator/CheckmateValidator.h
@@ -7,7 +7,7 @@
 #include "utility/utility.h"
 #include "entity/board/Board.h"
 #include "utility/check_validator/CheckValidator.h"
-
+#include "utility/move_validators/PawnMoveValidator.h"
 namespace Chess {
     class CheckmateValidator
     {
@@ -16,6 +16,9 @@ namespace Chess {
         // functions that can be used to make a temporary move
         void FakeMoveToNewPos(GridPosPtr& oldPos, GridPosPtr& newPos, Board& board);
         void FakeMoveFromNewPos(GridPosPtr& oldPos, GridPosPtr& newPos, Board& board);
+        static std::unique_ptr<std::vector<GridPosPtr>> GetAllPossiblePositionsForPiece(const std::shared_ptr<Piece> &piece, BoardMatrix& board);
+        static std::unique_ptr<std::vector<GridPosPtr>> GetAllPossibleHorizontalAndVerticalPositions(const std::shared_ptr<Piece> &piece, BoardMatrix& board);
+        static std::unique_ptr<std::vector<GridPosPtr>> GetAllPossibleDiagonalPositions(const std::shared_ptr<Piece> &piece, BoardMatrix& board);
     public:
         CheckmateValidator() = default;
         bool IsCheckmate(const std::shared_ptr<Piece>& king, Board &board);
